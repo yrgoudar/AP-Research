61c76d552b01631754bb67e8b3c68ddb02a5dc66
yrgoudar <yrgoudar@gmail.com>
2025-01-08 01:12:30 -0600

    edit firebase config

--- mushroom-simulation/src/firebase.js
+++ mushroom-simulation/src/firebase.js
@@ -20,7 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 // Initialize Firestore
 const db = getFirestore(app);
 
